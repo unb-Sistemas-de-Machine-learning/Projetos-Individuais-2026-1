f8eb6bd6cfa54e6b07e51bd56b22651b22d099de
lipeaaraujo <felipearaujodff@gmail.com>
2026-03-29 16:06:43 -0300

    refactor: using openlibrary api for book_fetcher

--- felipe-amorim-de-araujo/projeto-1/src/book_fetcher.py
+++ felipe-amorim-de-araujo/projeto-1/src/book_fetcher.py
@@ -1,40 +1,52 @@
-import os
 import requests
 from dotenv import load_dotenv
+from dataclasses import dataclass
 
 load_dotenv()
 
-GOOGLE_BOOKS_BASE_URL = "https://www.googleapis.com/books/v1/volumes"
-API_KEY = os.getenv("GOOGLE_BOOKS_API_KEY")
+# GOOGLE_BOOKS_BASE_URL = "https://www.googleapis.com/books/v1/volumes"
+OPENLIBRARY_SEARCH_URL = "https://openlibrary.org/search.json"
+# API_KEY = os.getenv("GOOGLE_BOOKS_API_KEY")
 
 
-def search_book_metadata(author_title: str) -> dict:
+@dataclass
+class Book:
+    title: str
+    authors: list[str]
+    categories: list[str]
+    description: str
+    isbn: str
+
+
+def search_book_metadata(author_title: str) -> Book:
     params = {
         "q": author_title,
         "maxResults": 1,
         "langRestrict": "pt",
         # "key": API_KEY
     }
-    response = requests.get(GOOGLE_BOOKS_BASE_URL, params, timeout=10)
+    response = requests.get(OPENLIBRARY_SEARCH_URL, params, timeout=10)
     response.raise_for_status()
     data = response.json()
 
-    if "items" not in data:
+    if not data.get("docs"):
         raise Exception(f"Book not found: {author_title}")
 
-    info = data["items"][0]["volumeInfo"]
-    return {
-        "title": info.get("title", author_title),
-        "authors": info.get("authors", []),
-        "categories": info.get("categories", []),
-        "description": info.get("description", ""),
-        "language": info.get("language", ""),
-        "isbn": _extract_isbn(info),
-    }
+    # info = data["items"][0]["volumeInfo"]
+    doc = data["docs"][0]
+    description = ""
+    fs = doc.get("first_sentence")
+    if isinstance(fs, dict):
+        description = fs.get("value", "")
+    elif isinstance(fs, str):
+        description = fs
 
+    book = Book(
+        doc.get("title", author_title),
+        doc.get("author_name", []),
+        doc.get("subject", [])[:5],
+        description,
+        doc.get("isbn", [""])[0] if doc.get("isbn") else "",
+    )
 
-def _extract_isbn(volume_info: dict) -> str:
-    for id_item in volume_info.get("industryIdentifiers", []):
-        if id_item["type"] in ("ISBN_13", "ISBN_10"):
-            return id_item["identifier"]
-    return ""
+    return book
