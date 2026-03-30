c1a1c88af4f89ada07f1bbd82318efe6866bc69f
Maciel Custódio <maciel_custodio@icloud.com>
2026-03-29 21:05:31 -0300

    :sparkles: feat: coletar e estruturar dados do perfil do GitHub
    - Buscar perfil do usuário
    - Buscar repositórios públicos com linguagem, estrelas e forks
    - Buscar linguagens mais utilizadas a partir dos repositórios
    - Buscar pull requests via search API

---
+++ maciel-ferreira-custodio-junior/projeto-1/main.py
@@ -0,0 +1,16 @@
+from github_client import get_full_profile
+
+username = input("Digite o username do GitHub: ")
+profile = get_full_profile(username)
+
+if profile:
+    print(f"Nome: {profile['name']}")
+    print(f"Conta criada em: {profile['created_at']}")
+    print(f"Repositórios públicos: {profile['public_repos']}")
+    print(f"Seguidores: {profile['followers']}")
+    print(f"Linguagens: {profile['languages'][:5]}")
+    print(f"Total de PRs abertos: {profile['pull_requests']['total_opened']}")
+    print(f"Total de PRs mergeados: {profile['pull_requests']['total_merged']}")
+    print(
+        f"Repos externos contribuídos: {profile['pull_requests']['external_repositories'][:5]}"
+    )
